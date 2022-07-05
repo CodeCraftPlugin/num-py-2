26a0259d11192cc2b00b247a29727db2cb61421b
CodeCraft Plugin <86466022+CodeCraftPlugin@users.noreply.github.com>
2022-07-05 14:43:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import numpy as ns
+
+a= ns.array([1,"codecratft",3])
+print(a)
+#Iterating Arrays.
+
+
+#2D array
+
+#We can iterate 2D arrays also.
+
+#Numpy Array Join
+
+#Join two 2-D arrays along rows (axis=1):
+
+#NumPy Array Slicing
+
+#Slice elements from index 4 to the end of the array:
+
+#Slice elements with step value:
+
+#Perform Arithmetic Operations on Array
+
+#Calculate square and sqrt of array elements:
+
+
